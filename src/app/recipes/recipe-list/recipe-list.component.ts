1e4efee4b1319c22deb6ce0690521cd5b5d69840
Joachim <joachim.stolle@hotmail.de>
2022-09-14 19:12:19 +0200

    added recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,9 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [new Recipe('Testrezept', 'Dies ist ein Test', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg')];
+  recipes: Recipe[] = [
+    new Recipe('Testrezept', 'Dies ist ein Test', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg'),
+    new Recipe('Testrezept 2', 'Dies ist ein neuer Test', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg')];
 
   constructor() { }
 
