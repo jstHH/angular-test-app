e6244d9e51b062c7182130cd0b077ef888ced2de
Joachim <joachim.stolle@hotmail.de>
2022-09-14 18:32:57 +0200

    added primitive navigation with event binding and ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-test-app';
+  pageSelected: string = 'recipes';
+
+  onPageChanged(page: string) {
+    this.pageSelected = page;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
+  @Output() pageChanged = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipesClick() {
+    this.pageChanged.emit('recipes');
+  }
+
+  onSchoppingListClick() {
+    this.pageChanged.emit('shopping');
+  }
 }
